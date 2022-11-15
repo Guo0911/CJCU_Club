46935ac87a87dfb3c576c767a750b6d8656531cf
Guo <109B30612@mailst.cjcu.edu.tw>
2022-11-15 20:00:00 +0800

    Update

--- 111-1/網路爬蟲經驗分享/2022_11_23/批踢踢實業坊文章列表爬取/WebCrawler.py
+++ 111-1/網路爬蟲經驗分享/2022_11_23/批踢踢實業坊文章列表爬取/WebCrawler.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 headers = {"cookie": "over18=1",
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"}
 
-url = input('請輸入批踢踢實業紡看板網址: ')
+url = input('請輸入批踢踢實業坊看板網址: ')
 
 request = requests.get(url, headers=headers)
 request.encoding = 'utf-8'
